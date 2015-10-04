169aaf7acf223674fa709ed2143eb6fd3b864bc2
Blair Gemmer <blair.gemmer@gmail.com>
2015-10-04 05:36:42 -0600

    Evin's new design, generated from Pipeline.py

--- js/fabricon.controllers.js
+++ js/fabricon.controllers.js
@@ -1,28 +1,171 @@
-var fabriconControllers = angular.module('fabriconControllers', []);
+var ui = angular.module('fabricon.controllers', []);
 
-var categories = null;
-var subcategories = null;
-fabriconControllers.controller('ContentController', ['$scope', '$http',
-    function($scope, $http) {
+ui.controller('NavigationBarController', ['$scope', '$state',
+    function($scope, $state) {
+        $(".nav li").on("click", function() {
+            $(".nav li").removeClass("active");
+            $(this).addClass("active");
+        });
+        
+        var nav_buttons = $(".nav li");
+        for (var i = 0; i < nav_buttons.length; i++) {
+            var nav_button = nav_buttons[i];
+
+            $(nav_button).removeClass("active");
+            if ($(nav_button).text().toUpperCase() === $state.current.name.toUpperCase()) {
+                $(nav_button).addClass("active");
+            }
+        }
+    }
+]);
+
+ui.controller('HomeController', ['$scope', '$state',
+    function($scope, $state) {
+        console.log("Home page!");   
+    }
+]);
+
+ui.controller('AboutController', ['$scope', '$state',
+    function($scope, $state) {
+        console.log("About page!");   
+    }
+]);
+
+ui.controller('ContactController', ['$scope', '$state',
+    function($scope, $state) {
+        console.log("Contact page!");   
+    }
+]);
+
+ui.controller('TypeController', ['$scope', '$state',
+    function($scope, $state) {
+        console.log("Type page!", $state);   
+    }
+]);
+
+ui.controller('StyleController', ['$scope', '$state',
+    function($scope, $state) {
+        console.log("Style page!", $state);   
+    }
+]);
+
+var split_Cap_join = function(thing, split_separator, join_separator) {
+    var thing_split = thing.split(split_separator);
+    for (var i = 0; i < thing_split.length; i++) {
+        if (thing_split[i][0] === undefined) { continue; }
+        thing_split[i] = thing_split[i][0].toUpperCase()+thing_split[i].slice(1, thing_split[i].length);
+    }
+    thing = thing_split.join(join_separator);
+
+    return thing;
+};
+
+var data_json_path = '../fabricon-test/home/home.json';
+var g_refresh_navigator = null;
+ui.controller('ProductsController', ['$rootScope', '$scope', '$http', '$state',
+    function($rootScope, $scope, $http, $state) {
+        console.log("Products page!");
+        console.log("$state:", $state);
+
+        var products_state_change_start = function(event, toState, toParams, fromState, fromParams) {
+            console.log("event:", event);
+            console.log("toState:", toState);
+            console.log("toParams:", toParams);
+            console.log("fromState:", fromState);
+            console.log("fromParams:", fromParams);
+
+            //+ Set Data Source based on application "state".
+            if ((toParams === null) || (toParams.type === undefined)) {
+                data_json_path = './home/home.json';
+            } else if (toParams !== fromParams) {
+                if (toParams.type !== undefined) {
+                    type = toParams.type;
+
+                    //+ Format Data URL.
+                    var type_directory = toParams.type;
+                    type_directory = split_Cap_join(type_directory, '-', '-');
+                    type_directory = split_Cap_join(type_directory, '_', '%20');
+                    type_directory = split_Cap_join(type_directory, ' ', '%20');
+                    //- Format Data URL.
+
+                    data_json_path = './home/'+type_directory+'/'+toParams.type+'.json';
+                }
+
+                if ((toParams.type !== undefined) && (toParams.style !== undefined)) {
+                    //+ Format Data URL.
+                    var type_directory = toParams.type;
+                    type_directory = split_Cap_join(type_directory, '-', '-');
+                    type_directory = split_Cap_join(type_directory, '_', '%20');
+                    type_directory = split_Cap_join(type_directory, ' ', '%20');
+                    //- Format Data URL.
+
+                    //+ Format Data URL.
+                    var style_directory = toParams.style;
+                    style_directory = split_Cap_join(style_directory, '-', '-');
+                    style_directory = split_Cap_join(style_directory, '_', '%20');
+                    style_directory = split_Cap_join(style_directory, ' ', '%20');
+                    //- Format Data URL.
+
+                    data_json_path = './home/'+type_directory+'/'+style_directory+'/'+toParams.style+'.json';    
+                }
+            } else {
+                data_json_path = './home/home.json';
+            }
+            //- Set Data Source based on application "state".
+
+            console.log(data_json_path);
+            g_refresh_navigator();
+        };
+
+        //+ This gets called every time the Products page is interacted with.
+        $rootScope.$on('$stateChangeStart', products_state_change_start);
+        //-
+
         $scope.main_gallery = {};
 
-        $http.get('./data/products.json').success(function(data) {
-            $scope.categories = data;
-            categories = data;
-        
-            $scope.main_gallery.images = {};
-            $scope.main_gallery.images.__full__ = {};
-            $scope.main_gallery.images.__full__.size = {};
-            $scope.main_gallery.images.__full__.size.height = 50;
-            $scope.main_gallery.images.__full__.size.width = 100;
-        
-            var gallery_images = [];
-            for (var i = 0; i < categories.length; i++) {
-                var row = categories[i];
+        var refresh_navigator = function(data) {
+            console.log("STATE TYPE:", $state.params.type);
+            console.log("STATE STYLE:", $state.params.style);
+            console.log("STATE TRAITS:", $state.params.traits);
+            $scope.tiles = data;
+
+            console.log($scope);
+
+            //+ Set tile properties.
+            for (var i = 0; i < $scope.tiles.length; i++) {
+                var row = $scope.tiles[i];
                 for (var j = 0; j < row.length; j++) {
-                    var category = row[j];
-                    var image_folder_path = category['image_folder_path'];
-                    var full_size_category_images = category.images.__full__.images;
+                    var tile = row[j];
+                    
+                    console.log("LINK NAME:", tile.link_name);
+                    tile.type = tile.link_name;
+
+                    if ($state.params.type !== undefined) {
+                        tile.type = $state.params.type;
+                        tile.style = tile.link_name;
+                    }
+
+                    row[j] = tile;
+                }
+            }
+            //- Set tile properties.
+
+            $scope.main_gallery.images = {};
+            $scope.main_gallery.images.full = {};
+            $scope.main_gallery.images.full.size = {};
+            $scope.main_gallery.images.full.size.height = 50;
+            $scope.main_gallery.images.full.size.width = 100;
+            
+            //+ Populate Main Gallery.
+            var gallery_images = [];
+            for (var i = 0; i < $scope.tiles.length; i++) {
+                var row = $scope.tiles[i];
+                for (var j = 0; j < row.length; j++) {
+                    var tile = row[j];
+
+                    var image_folder_path = tile['image_folder_path'];
+                    tile['image_folder_path'] = image_folder_path;
+                    var full_size_category_images = tile.images.full.images;
 
                     for (var k = 0; k < full_size_category_images.length; k++) {
                         var full_size_category_image_name = full_size_category_images[k];
@@ -37,46 +180,139 @@ fabriconControllers.controller('ContentController', ['$scope', '$http',
                 }
             }
 
-            $scope.main_gallery.images.__full__.images = gallery_images;
-        });
+            $scope.main_gallery.images.full.images = gallery_images;
+            //- Populate Main Gallery.
+        };
+
+        g_refresh_navigator = function() { 
+            $http.get(data_json_path).success(refresh_navigator);
+        };
+
+        console.log("Once: products_state_change_start");
+        products_state_change_start(null, $state, $state.params, $state, null);
     }
 ]);
 
-fabriconControllers.controller('CategoryController', ['$scope', '$routeParams', '$http',
-    function($scope, $routeParams, $http) {
-        $scope.main_gallery = {};
-        $scope.sub_gallery = {};
-        $scope.contents = {};
-
-        $scope.categoryName = $routeParams.categoryName;
-        $scope.categoryID = $routeParams.categoryID;
-
-        console.log("CATEGORIES", categories);
-        $scope.categories = categories;
-
-        //+ MODIFY BASED ON ROW #
-        var category_index = parseInt($scope.categoryID);
-        if (category_index < 3) {
-            $scope.category = $scope.categories[0][category_index];
-            $scope.category_thumbnails = $scope.categories[0][category_index].images['__thumbnail__'].images;
-        } else {
-            $scope.category = $scope.categories[1][category_index-3];
-            $scope.category_thumbnails = $scope.categories[1][category_index-3].images['__thumbnail__'].images;
+//+ DROPDOWN
+var configure_type_dropdown = function($state, categories) {
+    if (editing_type === true) {
+        $( '#sifter-type-container-dropdown' ).empty();
+        for (var i = 0; i < categories.length; i++) {
+            var category_row = categories[i];
+            
+            for (var j = 0; j < category_row.length; j++) {
+                var category = category_row[j];
+                $( '#sifter-type-container-dropdown' ).append('<li><a href="">'+category.name+'</a></li>')
+            }
         }
-        //- MODIFY BASED ON ROW #
-
-    	var data_url = $scope.category.data_url;
-        $http.get(data_url).success(function(data) {
-            $scope.subcategories = data;
-            subcategories = data;
-        });
-    }    
-]);
-
-fabriconControllers.controller('SubCategoryController', ['$scope', '$routeParams', '$http',
-    function($scope, $routeParams, $http) {
-        console.log("SCOPE", $scope);
-        console.log("ROUTE PARAMS", $routeParams);
-        console.log("HTTP", $http);
     }
-]);
+
+    if (type_click_set === true) { return; }
+
+    $( '#sifter-type-container-dropdown a' ).on( 'click', function( event ) {
+        var type_text = event.target.innerText;
+        console.log(type_text);
+        $('#sifter-type-button').text(type_text);
+        $("#sifter-type-button").css("font-weight", "bold");  
+        $('#sifter-type-button').append("&nbsp;<span class='caret'></span>");
+        
+        $('#sifter-style-button').text("Style");
+        $("#sifter-style-button").css("font-weight", "bold");  
+        $('#sifter-style-button').append("&nbsp;<span class='caret'></span>");
+        $("#sifter-style-button").prop("disabled", false);  
+    
+        $("#sifter-traits-button").text("Traits");
+        $("#sifter-traits-button").css("font-weight", "bold");  
+
+        reset_traits_dropdown();
+        $("#sifter-traits-button").prop("disabled", true);  
+    
+        data_json_path = '../fabricon-test/home/'+type_text+'/'+type_text.toLowerCase()+'.json';
+        console.log(data_json_path);
+        editing_type = false;
+        editing_style = true;
+
+        g_refresh_navigator();
+        $state.go('products.type', {'type': type_text.toLowerCase()});
+    });
+
+    type_click_set = true;
+};
+
+var configure_style_dropdown = function($state, categories) {
+    if (editing_style === true) {
+        $( '#sifter-style-container-dropdown' ).empty();
+        for (var i = 0; i < categories.length; i++) {
+            var category_row = categories[i];
+            
+            for (var j = 0; j < category_row.length; j++) {
+                var category = category_row[j];
+                $( '#sifter-style-container-dropdown' ).append('<li><a href="">'+category.name+'</a></li>')
+            }
+        }
+    }
+
+    if (style_click_set === true) { return; }
+  
+    $( '#sifter-style-container-dropdown a' ).on( 'click', function( event ) {
+        var style_text = event.target.innerText;
+        console.log(style_text);
+        $('#sifter-style-button').text(style_text);
+        $("#sifter-style-button").css("font-weight", "bold");  
+        $('#sifter-style-button').append("&nbsp;<span class='caret'></span>");
+        
+        $("#sifter-traits-button").text("Traits");
+        $("#sifter-traits-button").css("font-weight", "bold");  
+        $('#sifter-traits-button').append("&nbsp;<span class='caret'></span>");
+
+        reset_traits_dropdown();
+        $("#sifter-traits-button").prop("disabled", false); 
+    
+        $state.go('products.type.style', {'style': style_text.toLowerCase()});
+    });
+
+    style_click_set = true;
+};
+
+var configure_traits_dropdown = function($state) {
+    var options = [];
+ 
+    $( '#sifter-traits-container-dropdown a' ).on( 'click', function( event ) {
+
+       var $target = $( event.currentTarget ),
+           val = $target.attr( 'data-value' ),
+           $inp = $target.find( 'input' ),
+           idx;
+
+       if ( ( idx = options.indexOf( val ) ) > -1 ) {
+          options.splice( idx, 1 );
+          setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
+       } else {
+          options.push( val.toLowerCase() );
+          setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
+       }
+
+       $( event.target ).blur();
+          
+       console.log( options );
+      
+       $state.go('products.type.style.traits', {'traits': options});
+
+       return false;
+    });
+};
+
+var reset_traits_dropdown = function() {
+    var traits = $('#sifter-traits-container-dropdown').find("input[type=checkbox]");
+
+    for (var i = 0; i < traits.length; i++) {
+        var trait = traits[i];
+
+        if ($(trait).prop('checked')) {
+            $(trait).click();
+        }
+
+        console.log(trait);
+    }    
+};
+//- DROPDOWN
--- js/fabricon.js
+++ js/fabricon.js
@@ -1,42 +1,112 @@
 (function () {
-	var app = angular.module('fabricon', [
-    'ui.bootstrap',
-    'ngRoute',
-    'fabriconControllers'
-  ]);
+    var app = angular.module('fabricon', [
+        'fabricon.controllers',   //# Fabricon-specific behavior.
+        'ui.bootstrap',   //: Provides Twitter BootStrap compatibility.
+        'ui.router'   //: State-based routing of the application.
+    ]);
 	
-  app.config(['$routeProvider', 
-    function($routeProvider, $locationProvider) {
-        $routeProvider.
-            when('/home', {
-                templateUrl: './templates/front-page-content.html',
-                controller: 'ContentController'
-            }).
-            when('/:categoryName', {
-                templateUrl: './templates/front-page-content.html',
-                controller: 'ContentController'
-            }).
-            when('/:categoryName/:subcategoryName', {
-                templateUrl: './templates/category-content.html',
-                controller: 'CategoryController'
-            }).
-            when('/:categoryName/:subcategoryName/:productName', {
-                templateUrl: './templates/subcategory-product-content.html',
-                controller: 'ProductController'
-            }).
-            otherwise({
-                redirectTo: '/home'
-            });
-    }
-  ]);
+    app.config(function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise("/home");
 
-  //+ NAVIGATION BAR
-  app.directive("navigationBar", function() {
-    return {
-      restrict: "E",
-      templateUrl: "./templates/navigation-bar.html"
-    };
-  });
-  //- NAVIGATION BAR
-  
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                views: {
+                    'content': {
+                        templateUrl: './templates/home.html',
+                        controller: 'HomeController'
+                    }
+                }
+            })
+            .state('about', {
+                url: "/about",
+                views: {
+                    'content': {
+                        templateUrl: './templates/about.html',
+                        controller: 'AboutController'
+                    }
+                }
+            })
+            .state('contact', {
+                url: "/contact",
+                views: {
+                    'content': {
+                        templateUrl: './templates/contact.html',
+                        controller: 'ContactController'
+                    }
+                }
+            })
+            .state('products', {
+                url: "/products",
+                views: {
+                    'content': {
+                        templateUrl: './templates/products.html',
+                        controller: 'ProductsController'
+                    }
+                }
+            })
+            .state('type', {
+                url: "/:type",
+                views: {
+                    'content': {
+                        templateUrl: './templates/products.types.html',
+                        controller: 'ProductsController'
+                    }
+                }
+            })
+            .state('style', {
+                url: "/:type/:style",
+                views: {
+                    'content': {
+                        templateUrl: './templates/products.types.styles.html',
+                        controller: 'ProductsController'
+                    }
+                }
+            });
+            // .state('products.type.style', {
+            //     url: "/:style",
+            //     templateUrl: './templates/products.types.styles.html',
+            //     controller: 'ProductsController'
+            // })
+            // .state('products.traits', {
+            //     url: "/:traits",
+            //     templateUrl: './templates/traits.html',
+            //     controller: 'ProductsController'
+            // });
+        }
+    );
+
+    //+ NAVIGATION BAR
+    app.directive("navigationBar", function() {
+        return {
+            restrict: "E",
+            templateUrl: "./templates/navigation-bar.html",
+            controller: 'NavigationBarController'
+        };
+    });
+    //- NAVIGATION BAR
+
+    //+ FOOTER
+    app.directive("customFooter", function() {
+        return {
+            restrict: "E",
+            templateUrl: "./templates/custom-footer.html"
+        };
+    });  
+    //- FOOTER
+
+    //+ SITE MAP
+    app.directive("siteMap", function() {
+        return {
+            restrict: "E",
+            templateUrl: "./templates/site-map.html"
+        };
+    });  
+    app.directive("siteMapSocial", function() {
+        return {
+            restrict: "E",
+            templateUrl: "./templates/site-map-social.html"
+        };
+    });  
+//- SITE MAP
 })();
