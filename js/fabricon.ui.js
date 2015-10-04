f134e8060270ef8897cdab05523f34a1b30e5b0b
Blair Gemmer <blair.gemmer@gmail.com>
2015-10-04 06:14:23 -0600

    Resolving merge conflicts

---
+++ js/fabricon.ui.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	//Convert address tags to google map links - Michael Jasper 2012
+	$('address').each(function () {
+		var link = "<a href='http://maps.google.com/maps?q=" + encodeURIComponent( $(this).text() ) + "' target='_blank'>" + $(this).text() + "</a>";
+		$(this).html(link);
+	});
+	
+    $("map-address").each(function(){                         
+        var embed ="<iframe width='425' height='350' frameborder='0' scrolling='no'  marginheight='0' marginwidth='0' src='https://maps.google.com/maps?&amp;q="+ encodeURIComponent( $(this).text() ) +"&amp;output=embed'></iframe>";
+        $(this).html(embed);             
+    });
+})
